43e9e9d92e004d456297bbd7586548cc1d0c6c35
Adam Schønemann <adamschoenemann@gmail.com>
2016-10-31 22:13:19 +0100

    remove old scala project - it was a mess

--- eclipse/dk.itu.chomsky.configurator.scala/src/main/scala/dk/itu/chomsky/scala/internal/Main.scala
+++
@@ -1,9 +0,0 @@
-// // (c) mddbook, wasowski, tberger
-// // Executor for the FSM interpreter
-// // Run using 'sbt run' and select mdsebook.fsm.scala.internal.Main as the
-// // main class
-// package mdsebook.fsm.scala.internal
-
-// object Main extends App {
-//   mdsebook.fsm.scala.Interpreter.run (CoffeeMachine.m)
-// }
