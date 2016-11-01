88a1bbfd5ae520081ae7dddc41e58954953ce807
oscar_felipe <groovenino@gmail.com>
2016-11-01 12:43:15 +0100

    add readme with tasks and add a pedestrian navigation

--- android/Configurator3/app/src/main/java/dk/itu/chomsky/configurator/ArchitectureActivity.java
+++ android/Configurator3/app/src/main/java/dk/itu/chomsky/configurator/ArchitectureActivity.java
@@ -6,31 +6,35 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class CPUActivity extends AppCompatActivity {
+public class ArchitectureActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cpu);
+        setContentView(R.layout.activity_architecture);
+
+
 
         //setup the button
-        Button buttonMotherboard     = (Button) findViewById(R.id.buttonCpuMb);
-        Button buttonRam = (Button) findViewById(R.id.buttonCpuRam);
+        Button buttonMonitor     = (Button) findViewById(R.id.buttonMonitor);
+        Button buttonMotherboard = (Button) findViewById(R.id.buttonMotherboard);
+
+        buttonMonitor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent arch = new Intent(ArchitectureActivity.this,MainActivity.class);
+                startActivity(arch);
+            }
+        });
 
         buttonMotherboard.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent arch = new Intent(CPUActivity.this,ArchitectureActivity.class);
-                startActivity(arch);
-            }
-        });
-
-        buttonRam.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent arch = new Intent(CPUActivity.this,RamActivity.class);
+                Intent arch = new Intent(ArchitectureActivity.this,MotherboardActivity.class);
                 startActivity(arch);
             }
         });
     }
+
+
 }
