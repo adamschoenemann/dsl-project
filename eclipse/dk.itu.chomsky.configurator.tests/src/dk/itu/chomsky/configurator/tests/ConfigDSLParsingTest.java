180630802c7e8167b4e8ad9efe5b03d6de1c3e2d
Adam Schønemann <adamschoenemann@gmail.com>
2016-12-13 17:56:19 +0100

    add an extra test-case with groups

--- eclipse/dk.itu.chomsky.configurator.tests/src/dk/itu/chomsky/configurator/tests/ConfigDSLParsingTest.java
+++ eclipse/dk.itu.chomsky.configurator.tests/src/dk/itu/chomsky/configurator/tests/ConfigDSLParsingTest.java
@@ -32,97 +32,6 @@ import dk.itu.chomsky.configurator.validation.ConfigDSLValidator;
 @InjectWith(ConfigDSLInjectorProvider.class)
 public class ConfigDSLParsingTest {
 
-	final String input1 =
-		  "model foo \"foo\" {"
-		+ "   types {"
-		+ "      t1 \"Type 1\" {"
-		+ "          v1 \"Val1\""
-		+ "          v2 \"Val2\""
-		+ "      }"
-		+ "      t2 \"Type 2\" {"
-		+ "      }"
-		+ "   }"
-		+ "	  product bar \"bar\" {"
-		+ "      param baz \"baz\" Int"
-		+ "      param p2 \"Param 2\" t1"
-		+ "      constraints {"
-		+ "         constraint on baz \"fooconstraint\" !(5 + 10 > 0)"
-		+ "         constraint \"c2\" value($p2) == ((52 / 42 * 10) > 0)"
-		+ "         constraint \"c3\" 5 > 5"
-		+ "         constraint \"c4\" if (value($p2) > true) then false else true"
-		+ "         constraint \"c5\" contains(label($p2), \"foo\") == true"
-		+ "      }"
-		+ "   }"
-		+ "}";
-	
-	final String input2 = 
-		  "model computer_model \"Customize Your Computer\" {"
-		+ "  types {"
-		+ "    arch \"Architecture\" {"
-		+ "      x64 \"64 bit\""
-		+ "      x86 \"32 bit\""
-		+ "    }"
-		+ "    gpu \"GPU\" {"
-		+ "      gtx970 \"Asus Geforce GTX 970\""
-		+ "      gtx980 \"MSI Geforce GTX 980\""
-		+ "      rx480 \"Sapphire Radeon RX 840 NITRO+\""
-		+ "    }"
-		+ "    cpu \"CPU\" {"
-		+ "      i5 \"Intel 2.3 GHz i5\""
-		+ "      i7 \"Intel 2.2 GHz i7\""
-		+ "      x4 \"AMD 2.8 GHz Athlon X4\""
-		+ "    }"
-		+ "    mouse \"Mouse\" {"
-		+ "      razor \"Razord\""
-		+ "      logitech \"Logitech\""
-		+ "      microsoft \"Microsft\""
-		+ "    }"
-		+ "    keyboard \"Keyboard\" {"
-		+ "      ltkb \"Logitech\""
-		+ "      mskb \"Microsoft\""
-		+ "    }"
-		+ "  }"
-		+ "  product computer \"Computer\" {"
-		+ "    param arch \"Architecture\" arch"
-		+ "    param cpu \"CPU\" cpu"
-		+ "    param gpu \"GPU\" gpu"
-		+ "    param ram \"RAM\" Int"
-		+ "    param engraving \"Engraving\" Text"
-		+ "    param deliver \"Deliver\" Bool"
-		+ "    param ghz \"GHz\" Double"
-		+ ""
-		+ "    group \"Peripherals\" {"
-		+ "      param keyboard \"Keyboard\" keyboard optional"
-		+ "      param mouse \"Mouse\" mouse optional"
-		+ "    }"
-		+ "    constraints {"
-		+ "      constraint on ram \"Ram must be positive\" value($ram) > 0"
-		+ "      constraint on ram \"32-bits can at most have 4g ram\" if value($arch) == x86 then value($ram) <= 4 else true"
-		+ "      constraint on keyboard \"c1\" if value($gpu) == gtx970 then value($keyboard) == ltkb else true"
-		+ "      constraint \"c2\" if value($cpu) == i5 then value($gpu) == gtx970 || value($gpu) == gtx980 else true"
-		+ "      constraint on cpu \"CPU must be intel\" contains(label($cpu), \"Intel\")"
-		+ "    }"
-		+ "  }"
-		+ ""
-		+ "  group \"Displays\" {"
-		+ ""
-		+ "    group \"Inner group\" {"
-		+ " 		product lvl2_prod \"Level 2 Product\" {"
-		+ "             param lvl2p1 \"Level 2 Param\" Bool"
-		+ "             constraints {"
-		+ "                 constraint on lvl2p1 \"This must be true, thank you\" value($lvl2p1) == true"
-		+ "             }"
-		+ " 		}"	
-		+ "    }"
-		+ "    product monitor1 \"Monitor 1\" {"
-		+ "	       param inches1 \"Inches\" Double"
-		+ "    }"
-		+ "    product monitor2 \"Monitor 2\" {"
-		+ "	       param inches2 \"Inches\" Double"
-		+ "    }"
-		+ "  }"
-		+ "}";
-
 	@Inject	ParseHelper<Model> parser;
 	@Inject ValidationTestHelper validator;
 
@@ -240,7 +149,8 @@ public class ConfigDSLParsingTest {
 	@Test
 	public void testHtmlGen() {
 		try {
-			Chomsky.testHtmlGen(parser.parse(input2));
+			String input = readFile(new File("resources/input/positive/groups.cnfgdsl"));
+			Chomsky.testHtmlGen(parser.parse(input));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -288,27 +198,27 @@ public class ConfigDSLParsingTest {
 		} catch (Exception e) {}
 	}
 	
-	@Test
-	public void testJSExprGen() {
-		
-		try {
-			Model model = parser.parse(input1);
-			
-			EList<Constraint> constrs = ((Product)model.getChildren().get(0)).getConstraints();
-			System.out.println("Number of constraints: " + constrs.size());
-			for (Constraint cons : constrs) {
-				Expr expr = cons.getExpr();
-				if (expr == null) {
-					System.out.println(cons.getLabel() + "'s expression was null!");
-					continue;
-				}
-				String generated = Chomsky.genJSExpr(expr);
-				System.out.println(generated);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+//	@Test
+//	public void testJSExprGen() {
+//		
+//		try {
+//			Model model = parser.parse(input1);
+//			
+//			EList<Constraint> constrs = ((Product)model.getChildren().get(0)).getConstraints();
+//			System.out.println("Number of constraints: " + constrs.size());
+//			for (Constraint cons : constrs) {
+//				Expr expr = cons.getExpr();
+//				if (expr == null) {
+//					System.out.println(cons.getLabel() + "'s expression was null!");
+//					continue;
+//				}
+//				String generated = Chomsky.genJSExpr(expr);
+//				System.out.println(generated);
+//			}
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//	}
 	@Test 
 	public void loadModel() {
 		try {
